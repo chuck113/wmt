8f394d16fd0838bdc8a54fbdbaef9d7cb313fda6
chuck113 <c.kubicek@gmail.com>
2009-08-11 22:03:22 +0100

    removed old files

---
+++ src/com/where/gwt/client/Map.java
@@ -0,0 +1,12 @@
+package com.where.gwt.client;
+
+import com.google.gwt.core.client.EntryPoint;
+
+/**
+ * @author Charles Kubicek
+ */
+public class Map implements EntryPoint {
+    public void onModuleLoad() {
+        
+    }
+}
